c99272a2355d7e6cc8c10989a0cf9ca3fc24f63d
basil18 <raikhanzhetkerbek@gmail.com>
2024-03-13 11:24:48 -0500

    fixed

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "foo" {
   ami           = "ami-022661f8a4a1b91cf"
   instance_type = "t2.micro"
   tags = {
-      Name = "Raikhan-Instance-webhook"
+      Name = "Raikhan-Instance-webhook-1"
   }
 }
 
